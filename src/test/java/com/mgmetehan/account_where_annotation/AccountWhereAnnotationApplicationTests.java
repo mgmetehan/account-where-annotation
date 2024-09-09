bfd1813f88a856e3e4fdf5ba294a860fba05b665
mgmetehan <mgmetehan@hotmail.com>
2024-09-09 19:46:16 +0300

    init project

---
+++ src/test/java/com/mgmetehan/account_where_annotation/AccountWhereAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.account_where_annotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountWhereAnnotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
